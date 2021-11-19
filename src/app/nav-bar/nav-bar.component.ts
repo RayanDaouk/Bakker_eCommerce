f76cbfa81d242bc54b594d2789e408424fa41657
Rayan <daoukrayan02@gmail.com>
2021-11-19 09:43:32 +0100

    refactoring navBar

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -18,38 +18,5 @@ export class NavBarComponent implements OnInit {
   ngOnInit(): void {
     this.giftList = gift;
     console.log('giftList:', this.giftList);
-    // SPAWN MENU OF NAVIGATION
-    // TARGET NAV
-    this.nav1 = document.getElementById('nav_1');
-    this.menu1 = document.getElementById('menu_1');
-
-    this.nav2 = document.getElementById('nav_2');
-    this.menu2 = document.getElementById('menu_2');
-    // EVENT SPAWN
-    this.nav1.addEventListener('mouseover', () => {
-      this.menu1.style.display = 'block';
-    });
-    this.nav1.addEventListener('mouseout', () => {
-      this.menu1.style.display = 'none';
-    });
-    this.menu1.addEventListener('mouseover', () => {
-      this.menu1.style.display = 'block';
-    });
-    this.menu1.addEventListener('mouseout', () => {
-      this.menu1.style.display = 'none';
-    });
-
-    this.nav2.addEventListener('mouseover', () => {
-      this.menu2.style.display = 'flex';
-    });
-    this.nav2.addEventListener('mouseout', () => {
-      this.menu2.style.display = 'none';
-    });
-    this.menu2.addEventListener('mouseover', () => {
-      this.menu2.style.display = 'flex';
-    });
-    this.menu2.addEventListener('mouseout', () => {
-      this.menu2.style.display = 'none';
-    });
   }
 }
