6ccecd80221976990390a01f6cfe772d3f95582b
Rayan <daoukrayan02@gmail.com>
2021-11-23 09:28:33 +0100

    cleaning commentary

--- src/app/pages/collection/plantes-hydroponiques/plantes-hydroponiques.component.ts
+++ src/app/pages/collection/plantes-hydroponiques/plantes-hydroponiques.component.ts
@@ -16,9 +16,7 @@ export class PlantesHydroponiquesComponent implements OnInit {
   constructor(
     private datasService: DatasService,
     private _FilterService: FilterService
-  ) {
-    // this.savedCollection = this._DatasService.collection1;
-  }
+  ) {}
   ngOnInit(): void {
     this.getCollection();
   }
@@ -69,45 +67,6 @@ export class PlantesHydroponiquesComponent implements OnInit {
       : null;
     return this.collection;
   }
-
-  // getFilter(toggle: Array<boolean>) {
-  //   this.getCollection();
-  //   console.log('toggle', toggle, 'collection:', this.collection);
-  //   toggle[0]
-  //     ? (this.collection = this._FilterService.filteringPrice(
-  //         0,
-  //         19,
-  //         this.collection,
-  //         this.savedCollection
-  //       ))
-  //     : null;
-
-  //   toggle[1]
-  //     ? (this.collection.next = this._FilterService.filteringPrice(
-  //         20,
-  //         29,
-  //         this.collection,
-  //         this.savedCollection
-  //       ))
-  //     : null;
-  //   toggle[2]
-  //     ? (this.collection = this._FilterService.filteringPrice(
-  //         30,
-  //         39,
-  //         this.collection,
-  //         this.savedCollection
-  //       ))
-  //     : null;
-  //   toggle[3]
-  //     ? (this.collection = this._FilterService.filteringPrice(
-  //         40,
-  //         49,
-  //         this.collection,
-  //         this.savedCollection
-  //       ))
-  //     : null;
-  //   return this.collection;
-  // }
 
   removeCollection() {
     this.getCollection();
