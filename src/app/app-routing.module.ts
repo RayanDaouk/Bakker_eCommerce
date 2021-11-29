30cb5c6729c746169da5dcd218a8a45d7f9f66cd
Rayan <daoukrayan02@gmail.com>
2021-11-29 09:26:23 +0100

    Add dynamic URL and delete hard code in appRouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { PlantesHydroponiquesComponent } from './pages/collection/plantes-hydroponiques/plantes-hydroponiques.component';
-import { BouquetsFleursSecheesComponent } from './pages/collection/bouquets-fleurs-sechees/bouquets-fleurs-sechees.component';
+import { CollectionsComponent } from './pages/collections/collections.component';
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
   },
-  // {
-  //   path: 'collection/:name',
-  //   component: CollectionComponent,
-  // },
   {
-    path: 'collection/Plantes_hydroponiques',
-    component: PlantesHydroponiquesComponent,
-  },
-  {
-    path: 'collection/Bouquets_de_fleurs_sechees',
-    component: BouquetsFleursSecheesComponent,
+    path: 'collections/:name',
+    component: CollectionsComponent,
   },
 ];
 
